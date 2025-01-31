4129185a58da200a77ae09b75e233fd14de0d622
leleji <3996@163.com>
2025-01-31 17:34:44 +0800

    路径修改

--- str/ClipFlow.Desktop.MacOs/Program.cs
+++ str/ClipFlow.Desktop.MacOs/Program.cs
@@ -2,34 +2,34 @@
 using Avalonia.Media;
 using System;
 
-namespace ClipFlow.Desktop.MacOs
-{
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+namespace ClipFlow.Desktop.MacOs;
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace()
-                .With(new FontManagerOptions
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .With(new FontManagerOptions
+            {
+                DefaultFamilyName = "avares://Avalonia.Fonts.Inter/Assets#Inter",
+                FontFallbacks = new[]
                 {
-                    DefaultFamilyName = "avares://Avalonia.Fonts.Inter/Assets#Inter",
-                    FontFallbacks = new[]
-                    {
                             new FontFallback { FontFamily = "Microsoft YaHei UI" },
                             new FontFallback { FontFamily = "Noto Sans CJK SC" },
                             new FontFallback { FontFamily = "PingFang SC" },
                             new FontFallback { FontFamily = "Source Han Sans SC" },
                             new FontFallback { FontFamily = "WenQuanYi Micro Hei" }
-                    }
-                });
-    }
+                }
+            });
 }
+
